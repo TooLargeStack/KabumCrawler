6547e7e3558cf14e0688d2cf1bbfe28afe502d90
JPBeckner <pedrobeckner@hotmail.com>
2021-11-08 00:02:37 -0300

    wip: impl product flow spider

--- crawler/spiders/product_flow_spider.py
+++ crawler/spiders/product_flow_spider.py
@@ -2,13 +2,14 @@ from scrapy.http.request import Request
 from scrapy.responsetypes import Response
 import scrapy
 
-from models import Home, Category, Product
+from crawler.models import Home, Category, Product
 
 
 class ProductFlowSpiderSpider(scrapy.Spider):
     name = 'product_flow_spider'
     # allowed_domains = ['kabum.com.br']
-    start_urls = ['http://kabum.com.br/']
+    base_url = "https://www.kabum.com.br/"
+    start_urls = [base_url]
 
     def __init__(self, name=None, **kwargs):
         super().__init__(name=name, **kwargs)
@@ -19,11 +20,24 @@ class ProductFlowSpiderSpider(scrapy.Spider):
 
     def parse(self, response: Response):
         home = self.home_model(response)
-        for category_link in home.categories_links:
+        for category in home.categories:
             yield Request(
-                url=category_link,
-                callback=self.access_category
+                url=home.get_category_link(category=category),
+                callback=self.access_category,
+                headers={
+                    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Refer": self.base_url
+                },
+                meta={
+                    'dont_redirect': True
+                }
             )
 
     def access_category(self, response: Response):
-        pass
+        print(response.url)
+        text = response.text
+        category = self.category_model(response=response)
+        for page in category.pages:
+            pass
+
+# End Of File
